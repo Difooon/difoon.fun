d585474ed10a626273483ab704e64f59c277d9e5
DIFON <difoonofficial@gmail.com>
2024-09-22 17:00:36 +0500

    Delete src directory

--- src/js/title.js
+++
@@ -1,8 +0,0 @@
-let docTitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "DIFOON | Вернись :("; 
-})
-
-window.addEventListener("focus", () => {
-    document.title = docTitle;
-})
